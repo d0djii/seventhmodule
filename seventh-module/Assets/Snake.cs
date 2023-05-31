ca0c62fc26453e8b4a5f3a0e8a9dc41f2ef17028
GlooMxc <131756470+GlooMxc@users.noreply.github.com>
2023-05-31 19:18:56 +0500

    Grow Function

--- seventh-module/Assets/Snake.cs
+++ seventh-module/Assets/Snake.cs
@@ -6,6 +6,16 @@ public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
 
+    private List<Transform> _segments;
+
+    public Transform SegmentPrefab;
+
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
+
     private void Update()
     {
             if (Input.GetKeyDown(KeyCode.W)) {
@@ -20,10 +30,28 @@ public class Snake : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        for (int i=_segments.Count-1;i>0;i--)
+        {
+            _segments[i].position = _segments[i-1].position;
+        }
+
         this.transform.position = new Vector3(
              Mathf.Round(this.transform.position.x)+ _direction.x,
              Mathf.Round(this.transform.position.y) + _direction.y,
             0.0f
-        );
+        ); 
+    }
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.SegmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food") {
+            Grow();
+        }
     }
 }
