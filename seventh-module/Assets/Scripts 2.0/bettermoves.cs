c6a1378e2dd418ededa5e54aa083bad8f7ba2691
GlooMxc <131756470+GlooMxc@users.noreply.github.com>
2023-06-19 16:45:27 +0500

    bettermovement 2.0

--- seventh-module/Assets/Scripts 2.0/bettermoves.cs
+++ seventh-module/Assets/Scripts 2.0/bettermoves.cs
@@ -7,51 +7,65 @@ public class bettermoves : MonoBehaviour
     private bool isMoving;
     private Vector3 origPos, targetPos;
     private float timeToMove = 0.2f;
+
+    private Vector3 currentDirection = Vector3.zero; // Текущее направление движения
+
     void Awake()
     {
-        transform.position = new Vector3(1,1,1);
+        transform.position = new Vector3(1, 1, 1);
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.W) && !isMoving) {
-            StartCoroutine(MovePlayer(Vector3.up));
+        // Задаем текущее направление движения в зависимости от нажатой клавиши
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentDirection = Vector3.up;
         }
-        if(Input.GetKey(KeyCode.S) && !isMoving) {
-            StartCoroutine(MovePlayer(Vector3.down));
+        else if (Input.GetKey(KeyCode.S))
+        {
+            currentDirection = Vector3.down;
         }
-        if(Input.GetKey(KeyCode.A) && !isMoving) {
-            StartCoroutine(MovePlayer(Vector3.left));
+        else if (Input.GetKey(KeyCode.A))
+        {
+            currentDirection = Vector3.left;
         }
-        if(Input.GetKey(KeyCode.D) && !isMoving) {
-            StartCoroutine(MovePlayer(Vector3.right));
+        else if (Input.GetKey(KeyCode.D))
+        {
+            currentDirection = Vector3.right;
         }
-        
 
-
+        // Если персонаж не движется, запускаем корутину движения
+        if (!isMoving)
+        {
+            StartCoroutine(MovePlayer(currentDirection));
+        }
     }
 
-    private IEnumerator MovePlayer(Vector3 direction) {
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
         isMoving = true;
 
-        float ETime= 0;
+        float elapsedTime = 0;
         origPos = transform.position;
         targetPos = origPos + direction;
 
         transform.eulerAngles = new Vector3(0, 0, GetAngleFromVector(direction) - 90);
-        while(ETime < timeToMove) {
-            transform.position = Vector3.Lerp(origPos, targetPos, (ETime / timeToMove));
-            ETime += Time.deltaTime;
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
             yield return null;
-         }
+        }
         transform.position = targetPos;
-        
+
         isMoving = false;
     }
 
-    private float GetAngleFromVector(Vector3 d) {
+    private float GetAngleFromVector(Vector3 d)
+    {
         float n = Mathf.Atan2(d.y, d.x) * Mathf.Rad2Deg;
-        if(n < 0)
+        if (n < 0)
             n += 360;
         return n;
     }
