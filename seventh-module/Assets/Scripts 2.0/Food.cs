ff5953b1492d7453d32431dc6805349c99c32510
GlooMxc <131756470+GlooMxc@users.noreply.github.com>
2023-06-20 18:26:19 +0500

    1st stage pochti complete

--- seventh-module/Assets/Scripts 2.0/Food.cs
+++ seventh-module/Assets/Scripts 2.0/Food.cs
@@ -1,17 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Food : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public BoxCollider2D gridArea;
+    public int score;
+    [SerializeField] Text scoreText;
+    private void Start()
     {
+        RandomizePosition();
     }
 
-    // Update is called once per frame
+    public void RandomizePosition()
+    {
+        Bounds bounds = gridArea.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+
+        private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RandomizePosition();
+            score++;
+        }       
+    }
     void Update()
     {
-        
+        scoreText.text = score.ToString();
     }
 }
+
+
---
+++ seventh-module/Assets/Scripts 2.0/Timer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private int sec = 0; 
+    private int min = 0;
+    private TMP_Text _TimerText;
+    [SerializeField] private int delta = 0;
+
+    private void Start()
+    {
+        _TimerText = GameObject.Find("TimerText").GetComponent<TMP_Text>();
+        StartCoroutine(ITimer());
+    }
+
+    IEnumerator ITimer()
+    {
+        while(true)
+        {
+            if (sec==59)
+            {
+                min++;
+                sec=-1;
+            }
+            sec+=delta;
+            _TimerText.text= min.ToString("D2")+" : " + sec.ToString("D2");
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
