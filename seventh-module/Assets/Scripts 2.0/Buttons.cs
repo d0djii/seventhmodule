3394608cad53eec1cf8e4774eea49af7f1de393d
Alexey Sadovnikov <131872384+d0djii@users.noreply.github.com>
2023-06-23 09:47:28 +0500

    menu

--- seventh-module/Assets/Scripts 2.0/Buttons.cs
+++ seventh-module/Assets/Scripts 2.0/Buttons.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame() {
+        Application.Quit();
     }
 }
